0b9f2d63cc4137fbd46cbaf31dba9bae9f47d2ca
zrbcool <zhangrongbincool@163.com>
2020-03-28 20:56:54 +0800

    752. 打开转盘锁

--- src/main/java/leetcode/Solution_0752.java
+++ src/main/java/leetcode/Solution_0752.java
@@ -2,7 +2,7 @@ package leetcode;
 
 import java.util.*;
 
-class Solution {
+class Solution_0752 {
     public int openLock(String[] deadends, String target) {
         String begin = "0000";
         Set<String> deads = new HashSet<>(Arrays.asList(deadends));
@@ -16,24 +16,24 @@ class Solution {
         Queue<String> list = new LinkedList<>();
         list.offer(begin);
         while (!list.isEmpty()) {
-            String current = list.poll();
-            if (current == null) {
-                step++;
-                continue;
-            }
-            Queue<String> nexts = findNexts(current);
-            for (String next : nexts) {
-                if (deads.contains(next) || visited.contains(next))
-                    continue;
-                if (target.equals(next)) {
-                    return step;
+            int size = list.size();
+            for (int i = 0; i < size; i++) {
+                String current = list.poll();
+                Queue<String> nexts = findNexts(current);
+                for (String next : nexts) {
+                    if (deads.contains(next) || visited.contains(next))
+                        continue;
+                    if (target.equals(next)) {
+                        step++;
+                        return step;
+                    }
+                    visited.add(next);
+                    list.offer(next);
                 }
-                visited.add(next);
-                list.offer(next);
             }
-            list.offer(null);
+            step++;
         }
-        return step;
+        return -1;
     }
 
     private Queue<String> findNexts(String current) {
@@ -62,6 +62,6 @@ class Solution {
          */
         String[] deads = new String[]{"0201","0101","0102","1212","2002"};
         String target = "0202";
-        System.out.println(new Solution().openLock(deads, target));
+        System.out.println(new Solution_0752().openLock(deads, target));
     }
 }
